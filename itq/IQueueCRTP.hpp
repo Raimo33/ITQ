8d8d00aa76b738f8d198064ba4159517e9b5918a
Claudio <claudio.raimondi@protonmail.com>
2025-06-09 23:24:38 +0200

    Add interface

---
+++ itq/IQueueCRTP.hpp
@@ -0,0 +1,62 @@
+/*================================================================================
+
+File: IQueueCRTP.hpp                                                            
+Creator: Claudio Raimondi                                                       
+Email: claudio.raimondi@pm.me                                                   
+
+created at: 2025-05-12 18:01:10                                                 
+last edited: 2025-06-09 20:06:35                                                
+
+================================================================================*/
+
+#pragma once
+
+#include <cstddef>
+#include <atomic>
+#include <array>
+
+#ifndef CACHELINE_SIZE
+# define CACHELINE_SIZE 64
+#endif
+
+template <size_t N>
+concept is_power_of_two = (N > 0) && ((N & (N - 1)) == 0);
+
+namespace itq
+{
+
+template <typename Derived, typename Item, size_t Capacity>
+  requires is_power_of_two<Capacity>
+class IQueueCRTP
+{
+  public:
+    IQueueCRTP(void) = default;
+    ~IQueueCRTP(void) = default;
+
+    template <typename ForwardItem>
+    inline void push(ForwardItem &&item) noexcept {
+      derived()->push_impl(std::forward<ForwardItem>(item));
+    }
+
+    template <typename... Args>
+    inline void emplace(Args &&...args) noexcept {
+      derived()->emplace_impl(std::forward<Args>(args)...);
+    }
+
+    inline bool pop(Item &out) noexcept {
+      return derived()->pop_impl(out);
+    }
+
+  protected:
+    static constexpr size_t wrap_mask = Capacity - 1;
+
+    alignas(CACHELINE_SIZE) std::atomic<size_t> write_idx;
+    alignas(CACHELINE_SIZE) std::atomic<size_t> read_idx;
+    alignas(CACHELINE_SIZE) std::array<Item, Capacity> buffer;
+
+  private:
+    inline Derived *derived(void) noexcept { return static_cast<Derived*>(this); }
+    inline const Derived *derived(void) const noexcept { return static_cast<const Derived*>(this); }
+};
+
+}
